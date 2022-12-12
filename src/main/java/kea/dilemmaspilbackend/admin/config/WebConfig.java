374d3c65de8be1f2532ccc198ad55fef5b308ec6
Danish-Pirate <arja0015@stud.kea.dk>
2022-12-12 13:49:37 +0100

    Testing cors

--- src/main/java/kea/dilemmaspilbackend/admin/config/WebConfig.java
+++ src/main/java/kea/dilemmaspilbackend/admin/config/WebConfig.java
@@ -11,6 +11,6 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**").allowedOrigins("https://lively-pond-0744c3603.2.azurestaticapps.net/**");
     }
 }
