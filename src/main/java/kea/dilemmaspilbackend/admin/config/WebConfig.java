964b6699079b67ceb1e2d0969d68edfcf52f6ed7
Danish-Pirate <arja0015@stud.kea.dk>
2022-12-12 13:15:03 +0100

    Updated cors, again

--- src/main/java/kea/dilemmaspilbackend/admin/config/WebConfig.java
+++ src/main/java/kea/dilemmaspilbackend/admin/config/WebConfig.java
@@ -2,14 +2,15 @@ package kea.dilemmaspilbackend.admin.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/https://lively-pond-0744c3603.2.azurestaticapps.net/*")
-                .allowedMethods("*");
+        registry.addMapping("/**");
     }
 }
